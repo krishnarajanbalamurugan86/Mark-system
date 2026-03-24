fed0f4bd816cf2fe3d46566f671fb84c5e0c3536
krishnarajanbalamurugan86 <krishnarajanbalamurugan86@gmail.com>
2026-03-24 13:12:24 +0530

    added public

--- backend/express-server.js
+++ backend/express-server.js
@@ -35,8 +35,8 @@ app.use((req, res, next) => {
 });
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/index.html"));
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/dist/index.html"));
 });
 
 // Serve frontend-vanilla statically (already existing usage of express.static)
