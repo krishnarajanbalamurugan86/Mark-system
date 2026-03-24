6a469a4c394ea628f2dedb6a1ac909c8076431ab
krishnarajanbalamurugan86 <krishnarajanbalamurugan86@gmail.com>
2026-03-24 13:16:20 +0530

    added public

--- backend/express-server.js
+++ backend/express-server.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
 });
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/*", (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, "public/dist/index.html"));
 });
 
